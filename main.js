15d9f4e6b6aca4d84de8c415d9438e6f793cb1e5
fiza _nawaz <162031014+fizza-nawaz@users.noreply.github.com>
2024-03-14 23:03:41 +0500

    Add files via upload
    Add an optional added description

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+// storing a person name in avariable
+let personname = "Fiza Nawaz";
+//printing a person name in lowercase
+console.log("Lowercase:", personname.toLowerCase());
+// printing a person name in uppercase
+console.log("Uppercase:", personname.toUpperCase());
+//printing a person name in titlecase
+console.log("Titlecase:", personname.toTitleCase());
---
+++ main.ts
@@ -0,0 +1,14 @@
+// storing a person name in avariable
+
+let personname:string = "Fiza Nawaz"
+
+//printing a person name in lowercase
+
+console.log("Lowercase:", personname.toLowerCase());
+
+// printing a person name in uppercase
+console.log("Uppercase:",personname.toUpperCase());
+
+//printing a person name in titlecase
+
+console.log("Titlecase:",personname.toTitleCase());
